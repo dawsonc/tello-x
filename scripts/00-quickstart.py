06308ad39d37a0d5b2d2a730737048ce0171389e
Charles Dawson <charles.dwsn@gmail.com>
2023-08-11 19:29:59 -0400

    Fix 00 script

--- scripts/00-quickstart.py
+++ scripts/00-quickstart.py
@@ -1,7 +1,10 @@
 """A minimal example of using the tellox library."""
+import time
+
 import tellox as tx
 
 # Make a pilot object, take off, and land
 pilot = tx.Pilot()
 pilot.takeoff()
+time.sleep(2)
 pilot.land()
